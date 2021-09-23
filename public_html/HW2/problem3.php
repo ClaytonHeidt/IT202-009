1f5b4b7f7e19de6689f05980eb4e741d7f80b05c
ClaytonHeidt <cjh47@njit.edu>
2021-09-23 06:16:46 -0400

    finshed number 3!

--- public_html/HW2/problem3.php
+++ public_html/HW2/problem3.php
@@ -8,7 +8,10 @@ function bePositive($arr) {
     echo "<br>Processing Array:<br><pre>" . var_export($arr, true) . "</pre>";
     echo "<br>Positive output:<br>";
     //TODO use echo to output all of the values as positive (even if they were originally positive)
-
+    for ($x = 0; $x < count($arr); $x++){
+        $y = abs($arr[$x]);
+        echo $y . "<br>";
+    }
 }
 echo "Problem 3: Be Positive<br>";
 ?>
