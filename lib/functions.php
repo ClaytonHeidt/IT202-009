b4539b36e6fa829310a0f38d5ce54b1ee9a25ef2
ClaytonHeidt <cjh47@njit.edu>
2021-11-09 13:31:33 -0500

    updated is_logged_in() helper function in functions.php

--- lib/functions.php
+++ lib/functions.php
@@ -36,9 +36,14 @@ function is_valid_email($email = "")
     return filter_var(trim($email), FILTER_VALIDATE_EMAIL);
 }
 //TODO 3: User Helpers
-function is_logged_in()
+function is_logged_in($redirect = false, $destination = "login.php")
 {
-    return isset($_SESSION["user"]); //se($_SESSION, "user", false, false);
+    $isLoggedIn = isset($_SESSION["user"]);
+    if ($redirect && !$isLoggedIn) {
+        flash("You must be logged in to view this page", "warning");
+        die(header("Location: $destination"));
+    }
+    return $isLoggedIn; //se($_SESSION, "user", false, false);
 }
 function has_role($role)
 {
