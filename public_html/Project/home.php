4831053b969f77d7b83102ec86e15e6c8530c5ce
ClaytonHeidt <cjh47@njit.edu>
2021-11-11 01:18:14 -0500

    Hid the debug output in home.php

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -1,15 +1,17 @@
 <?php
 require(__DIR__ . "/../../partials/nav.php");
 ?>
-<h1>Home</h1>
-<?php
+<div class="container-fluid">
+    <h1>Home</h1>
+    <?php
 
-if (is_logged_in(true)) {
-    echo "Welcome home, " . get_username();
-    //comment this out if you don't want to see the session variables
-    echo "<pre>" . var_export($_SESSION, true) . "</pre>";
-}
-?>
+    if (is_logged_in(true)) {
+        echo "Welcome home, " . get_username();
+        //comment this out if you don't want to see the session variables
+        //echo "<pre>" . var_export($_SESSION, true) . "</pre>";
+    }
+    ?>
+</div>
 <?php
 require(__DIR__ . "/../../partials/flash.php");
 ?>
