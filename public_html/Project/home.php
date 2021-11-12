9a812e22dac2da78fce8225b2cee4476383c3fa6
ClaytonHeidt <cjh47@njit.edu>
2021-11-12 00:44:32 -0500

    Properly displays the user's role

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -10,6 +10,17 @@ require(__DIR__ . "/../../partials/nav.php");
         //comment this out if you don't want to see the session variables
         //echo "<pre>" . var_export($_SESSION, true) . "</pre>";
     }
+    
+    //Tells the user what their current role is
+    $db = getDB();
+    $stmt = $db->prepare("SELECT name FROM Roles WHERE id=?");
+    $stmt->execute([get_user_id()]);
+    $role = $stmt->fetchColumn();
+    if (!$role == "") {
+        echo "<br>Your current role is ";
+        print_r($role);
+    }
+
     ?>
 </div>
 <?php
