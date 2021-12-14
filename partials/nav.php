afaba37081953c85ce6b3c1e8a3e471c20628f3f
ClaytonHeidt <cjh47@njit.edu>
2021-12-13 22:01:44 -0500

    Updated nav.php so user can easily navigate to create a competition

--- partials/nav.php
+++ partials/nav.php
@@ -39,7 +39,15 @@ require_once(__DIR__ . "/../lib/functions.php");
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <?php if (is_logged_in()) : ?>
                     <li class="nav-item"><a class="nav-link" href="<?php echo get_url('profile.php'); ?>">Profile</a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?php echo get_url('game.php'); ?>">Play Game!</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?php echo get_url('game.php'); ?>">Play Star Shooter!</a></li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="competitionsDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Competitions</a>
+                        <ul class="dropdown-menu bg-warning" aria-labelledby="competitionsDropdown">
+                            <li><a class="dropdown-item" href="<?php echo get_url('new_competition.php'); ?>">New Competition</a></li>
+                            <li><a class="dropdown-item" href="<?php echo get_url('list_competition.php'); ?>">List Competitions</a></li>
+                        </ul>
+                    </li>
+                    
                 <?php endif; ?>
                 <?php if (!is_logged_in()) : ?>
                     <li class="nav-item"><a class="nav-link" href="<?php echo get_url('login.php'); ?>">Login</a></li>
@@ -51,9 +59,9 @@ require_once(__DIR__ . "/../lib/functions.php");
                             Admin Roles
                         </a>
                         <ul class="dropdown-menu bg-warning" aria-labelledby="rolesDropdown">
-                            <li><a class="dropdown-item" href="<?php echo get_url('admin/create_role.php'); ?>">Create</a></li>
-                            <li><a class="dropdown-item" href="<?php echo get_url('admin/list_roles.php'); ?>">List</a></li>
-                            <li><a class="dropdown-item" href="<?php echo get_url('admin/assign_roles.php'); ?>">Assign</a></li>
+                            <li><a class="dropdown-item" href="<?php echo get_url('admin/create_role.php'); ?>">Create Role</a></li>
+                            <li><a class="dropdown-item" href="<?php echo get_url('admin/list_roles.php'); ?>">List Roles</a></li>
+                            <li><a class="dropdown-item" href="<?php echo get_url('admin/assign_roles.php'); ?>">Assign Roles</a></li>
                         </ul>
                     </li>
                 <?php endif; ?>
