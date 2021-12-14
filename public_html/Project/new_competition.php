744f74aeed33e73cd11459c43482535ec8e2e4c1
ClaytonHeidt <cjh47@njit.edu>
2021-12-13 21:46:55 -0500

    Updated some values

--- public_html/Project/new_competition.php
+++ public_html/Project/new_competition.php
@@ -1,20 +1,7 @@
 <?php
 require_once(__DIR__ . "/../../partials/nav.php");
 is_logged_in(true);
-/*$payout_options = [];
-$db = getDB();
-$stmt = $db->prepare("SELECT id, CONCAT(first_place,'% - ', second_place, '% - ', third_place, '%') as place FROM BGD_Payout_Options");
-try {
-    $stmt->execute();
-    $r = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    if ($r) {
-        $payout_options = $r;
-    }
-} catch (PDOException $e) {
-    flash("There was a problem fetching first, second, third place options", "danger");
-    error_log("Error Getting Places: " . var_export($e, true));
-}*/
-//save
+
 /*if (isset($_POST["title"]) && !empty($_POST["title"])) {
     $cost = (int)se($_POST, "starting_reward", 0, false);
     $cost++;
@@ -50,12 +37,12 @@ try {
     <h1>New Competition</h1>
     <form method="POST">
         <div class="mb-3">
-            <label for="title" class="form-label">Competition Name</label>
-            <input id="title" name="title" class="form-control" placeholder="My Competition"/>
+            <label for="cname" class="form-label">Competition Name</label>
+            <input id="cname" name="name" class="form-control" placeholder="My Competition"/>
         </div>
         <div class="mb-3">
-            <label for="reward" class="form-label">Starting Reward</label>
-            <input id="reward" type="number" name="starting_reward" class="form-control" onchange="updateCost()" placeholder=">= 1" min="1" />
+            <label for="sreward" class="form-label">Starting Reward</label>
+            <input id="sreward" type="number" name="starting_reward" class="form-control" onchange="updateCost()" placeholder=">= 1" min="1" />
         </div>
         <div class="mb-3">
             <label for="ms" class="form-label">Minimum Score (That Qualifies)</label>
@@ -66,23 +53,27 @@ try {
             <input id="mp" name="min_participants" type="number" class="form-control" placeholder=">= 3" min="3" />
         </div>
         <div class="mb-3">
-            <label for="jc" class="form-label">Cost to Join</label>
-            <input id="jc" name="join_cost" type="number" class="form-control" onchange="updateCost()" placeholder=">= 0" min="0" />
+            <label for="jf" class="form-label">Fee to Join</label>
+            <input id="jf" name="join_fee" type="number" class="form-control" onchange="updateCost()" placeholder=">= 0" min="0" />
         </div>
         <div class="mb-3">
-            <label for="duration" class="form-label">Competition Duration (in Days)</label>
-            <input id="duration" name="duration" type="number" class="form-control" placeholder=">= 3" min="3" />
+            <label for="dur" class="form-label">Competition Duration (in Days)</label>
+            <input id="dur" name="duration" type="number" class="form-control" placeholder=">= 3" min="3" />
         </div>
         <div class="mb-3">
-            <label for="po" class="form-label">Reward Distribution (1st, 2nd, and 3rd place)</label>
-            <select id="po" name="payout_option" class="form-control">
-                <?php foreach ($payout_options as $po) : ?>
-                    <option value="<?php se($po, 'id'); ?>"><?php se($po, 'place'); ?></option>
-                <?php endforeach; ?>
-            </select>
+            <label for="fpp" class="form-label">Reward Percentage for 1st place (out of 100 %)</label>
+            <input id="fpp" name="first_place_per" type="number" class="form-control" placeholder="60 %" />
         </div>
         <div class="mb-3">
-            <input type="submit" value="Create Competition" class="btn btn-primary" />
+            <label for="spp" class="form-label">Reward Percentage for 2nd place (out of 100 %)</label>
+            <input id="spp" name="second_place_per" type="number" class="form-control" placeholder="30 %" />
+        </div>
+        <div class="mb-3">
+            <label for="tpp" class="form-label">Reward Percentage for 3rd place (out of 100 %)</label>
+            <input id="tpp" name="third_place_per" type="number" class="form-control" placeholder="10 %" />
+        </div>
+        <div class="mb-3">
+            <input type="submit" value="Create Competition (Point Cost = 1 + Starting Reward)" class="btn btn-primary" />
         </div>
     </form>
     <script>
