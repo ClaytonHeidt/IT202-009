ac562135f43e37b8bb511fca16ed489f3d77301e
ClaytonHeidt <cjh47@njit.edu>
2021-12-15 02:24:15 -0500

    Fixed up data within calc_winners function

--- lib/functions.php
+++ lib/functions.php
@@ -501,9 +501,8 @@ function calc_winners()
     $db = getDB();
     elog("Starting winner calc");
     $calced_comps = [];
-    $stmt = $db->prepare("select c.id,c.title, first_place, second_place, third_place, current_reward 
-    from BGD_Competitions c JOIN BGD_Payout_Options po on c.payout_option = po.id 
-    where expires <= CURRENT_TIMESTAMP() AND did_calc = 0 AND current_participants >= min_participants LIMIT 10");
+    $stmt = $db->prepare("select c.id, c.name, first_place_per, second_place_per, third_place_per, current_reward from Competitions c 
+    where expires <= CURRENT_TIMESTAMP() AND current_participants >= min_participants LIMIT 10");
     try {
         $stmt->execute();
         $r = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -511,11 +510,11 @@ function calc_winners()
             $rc = $stmt->rowCount();
             elog("Validating $rc comps");
             foreach ($r as $row) {
-                $fp = floatval(se($row, "first_place", 0, false) / 100);
-                $sp = floatval(se($row, "second_place", 0, false) / 100);
-                $tp = floatval(se($row, "third_place", 0, false) / 100);
+                $fp = floatval(se($row, "first_place_per", 0, false) / 100);
+                $sp = floatval(se($row, "second_place_per", 0, false) / 100);
+                $tp = floatval(se($row, "third_place_per", 0, false) / 100);
                 $reward = (int)se($row, "current_reward", 0, false);
-                $title = se($row, "title", "-", false);
+                $title = se($row, "name", "-", false);
                 $fpr = ceil($reward * $fp);
                 $spr = ceil($reward * $sp);
                 $tpr = ceil($reward * $tp);
@@ -526,23 +525,20 @@ function calc_winners()
                     if ($r) {
                         $atleastOne = false;
                         foreach ($r as $index => $row) {
-                            $aid = se($row, "account_id", -1, false);
+                            //$aid = se($row, "account_id", -1, false);
                             $score = se($row, "score", 0, false);
                             $user_id = se($row, "user_id", -1, false);
                             if ($index == 0) {
-                                if (change_bills($fpr, "won-comp", -1, $aid, "First place in $title with score of $score")) {
-                                    $atleastOne = true;
-                                }
+                                update_points($fpr, "Comp_1st_Place");
+                                $atleastOne = true;
                                 elog("User $user_id First place in $title with score of $score");
                             } else if ($index == 1) {
-                                if (change_bills($spr, "won-comp", -1, $aid, "Second place in $title with score of $score")) {
-                                    $atleastOne = true;
-                                }
+                                update_points($fpr, "Comp_2nd_Place");
+                                $atleastOne = true;
                                 elog("User $user_id Second place in $title with score of $score");
-                            } else if ($index == 1) {
-                                if (change_bills($tpr, "won-comp", -1, $aid, "Third place in $title with score of $score")) {
-                                    $atleastOne = true;
-                                }
+                            } else if ($index == 2) {
+                                update_points($fpr, "Comp_3rd_Place");
+                                $atleastOne = true;
                                 elog("User $user_id Third place in $title with score of $score");
                             }
                         }
@@ -564,7 +560,7 @@ function calc_winners()
     }
     //closing calced comps
     if (count($calced_comps) > 0) {
-        $query = "UPDATE BGD_Competitions set did_calc = 1 AND did_payout = 1 WHERE id in ";
+        $query = "UPDATE Competitions set paid_out = 1 WHERE id in ";
         $query = "(" . str_repeat("?,", count($calced_comps) - 1) . "?)";
         elog("Close query: $query");
         $stmt = $db->prepare($query);
@@ -579,13 +575,13 @@ function calc_winners()
         elog("No competitions to calc");
     }
     //close invalid comps
-    $stmt = $db->prepare("UPDATE BGD_Competitions set did_calc = 1 WHERE expires <= CURRENT_TIMESTAMP() AND current_participants < min_participants AND did_calc = 0");
+    /*$stmt = $db->prepare("UPDATE Competitions set did_calc = 1 WHERE expires <= CURRENT_TIMESTAMP() AND current_participants < min_participants AND did_calc = 0");
     try {
         $stmt->execute();
         $rows = $stmt->rowCount();
         elog("Closed $rows invalid competitions");
     } catch (PDOException $e) {
         error_log("Closing invalid comps error: " . var_export($e, true));
-    }
+    }*/
     elog("Done calc winners");
 }
