62cb884b9efd534281aacb83ac80a730289437c3
ClaytonHeidt <cjh47@njit.edu>
2021-12-15 01:46:20 -0500

    updated and cleaned up view_competition

--- public_html/Project/view_competition.php
+++ public_html/Project/view_competition.php
@@ -6,18 +6,22 @@ $db = getDB();
 if (isset($_POST["join"])) {
     $user_id = get_user_id();
     $comp_id = se($_POST, "comp_id", 0, false);
-    $cost = se($_POST, "join_cost", 0, false);
+    $cost = se($_POST, "join_fee", 0, false);
     join_competition($comp_id, $user_id, $cost);
 }
 $id = se($_GET, "id", -1, false);
 if ($id < 1) {
     flash("Invalid competition", "danger");
-    redirect("list_competitions.php");
+    redirect("active_competitions.php");
 }
 //handle page load
-$stmt = $db->prepare("SELECT BGD_Competitions.id , title, min_participants, current_participants, current_reward, expires, creator_id, min_score, join_cost, IF(competition_id is null, 0, 1) as joined,  CONCAT(first_place,'% - ', second_place, '% - ', third_place, '%') as place FROM BGD_Competitions
+/*$stmt = $db->prepare("SELECT BGD_Competitions.id , title, min_participants, current_participants, current_reward, expires, creator_id, min_score, join_cost, IF(competition_id is null, 0, 1) as joined,  CONCAT(first_place,'% - ', second_place, '% - ', third_place, '%') as place FROM BGD_Competitions
 JOIN BGD_Payout_Options on BGD_Payout_Options.id = BGD_Competitions.payout_option
-LEFT JOIN (SELECT * FROM BGD_UserComps where BGD_UserComps.user_id = :uid) as t on t.competition_id = BGD_Competitions.id WHERE BGD_Competitions.id = :cid");
+LEFT JOIN (SELECT * FROM BGD_UserComps where BGD_UserComps.user_id = :uid) as t on t.competition_id = BGD_Competitions.id WHERE BGD_Competitions.id = :cid");*/
+
+$stmt = $db->prepare("SELECT Competitions.id, name, min_participants, current_participants, current_reward, expires, min_score, join_fee, IF(comp_id is null, 0, 1) as joined, CONCAT(first_place_per,'% - ', second_place_per, '% - ', third_place_per, '%') as place FROM Competitions
+LEFT JOIN (SELECT * FROM CompetitionParticipants WHERE CompetitionParticipants.user_id = :uid) as t ON t.comp_id = Competitions.id WHERE Competitions.id = :cid");
+
 $row = [];
 $comp = "";
 try {
@@ -25,7 +29,7 @@ try {
     $r = $stmt->fetch();
     if ($r) {
         $row = $r;
-        $comp = se($r, "title", "N/A", false);
+        $comp = se($r, "name", "N/A", false);
     }
 } catch (PDOException $e) {
     flash("There was a problem fetching competitions, please try again later", "danger");
@@ -37,16 +41,16 @@ $scores = get_top_scores_for_comp($id);
     <h1>View Competition: <?php se($comp); ?></h1>
     <table class="table text-light">
         <thead>
-            <th>Title</th>
+            <th>Competition Name</th>
             <th>Participants</th>
             <th>Reward</th>
-            <th>Min Score</th>
+            <th>Minimum Score</th>
             <th>Expires</th>
             <th>Actions</th>
         </thead>
         <tbody>
             <?php if (count($row) > 0) : ?>
-                <td><?php se($row, "title"); ?></td>
+                <td><?php se($row, "name"); ?></td>
                 <td><?php se($row, "current_participants"); ?>/<?php se($row, "min_participants"); ?></td>
                 <td><?php se($row, "current_reward"); ?><br>Payout: <?php se($row, "place", "-"); ?></td>
                 <td><?php se($row, "min_score"); ?></td>
@@ -57,14 +61,14 @@ $scores = get_top_scores_for_comp($id);
                     <?php else : ?>
                         <form method="POST">
                             <input type="hidden" name="comp_id" value="<?php se($row, 'id'); ?>" />
-                            <input type="hidden" name="cost" value="<?php se($row, 'join_cost', 0); ?>" />
-                            <input type="submit" name="join" class="btn btn-primary" value="Join (Cost: <?php se($row, "join_cost", 0) ?>)" />
+                            <input type="hidden" name="cost" value="<?php se($row, 'join_fee', 0); ?>" />
+                            <input type="submit" name="join" class="btn btn-primary" value="Join (Cost: <?php se($row, "join_fee", 0) ?>)" />
                         </form>
                     <?php endif; ?>
                 </td>
             <?php else : ?>
                 <tr>
-                    <td colspan="100%">No active competitins</td>
+                    <td colspan="100%">No active competitions</td>
                 </tr>
             <?php endif; ?>
         </tbody>
@@ -76,5 +80,5 @@ $scores = get_top_scores_for_comp($id);
     ?>
 </div>
 <?php
-require(__DIR__ . "/../../partials/footer.php");
+require(__DIR__ . "/../../partials/flash.php");
 ?>
